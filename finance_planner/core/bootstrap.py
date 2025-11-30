494aeb3d79b3715d6233aee1a5bb0c387c6857e0
ewasince <ewasince@gmail.com>
2025-11-30 23:59:13 +0300

    change bootstrap

--- finance_planner/core/bootstrap.py
+++ finance_planner/core/bootstrap.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from datetime import UTC, date, datetime
+from datetime import UTC, date, datetime, timedelta
 from typing import Any, Final
 
 from accounts.models import Account, AccountType
@@ -42,22 +42,28 @@ def _ensure_success(response, *, action: str) -> None:
 
 class Bootstraper:
     def __init__(self) -> None:
-        pass
+        self.test_client: APIClient | None = None
+        self.demo_client: APIClient | None = None
+
+        self.test_salary_scenario_id: str | None = None
+        self.demo_advance_scenario_id: str | None = None
+        self.demo_salary_scenario_id: str | None = None
+        self.demo_scholarship_scenario_id: str | None = None
 
     def bootstrap(self) -> None:
         call_command("migrate", interactive=False, verbosity=0)
         with transaction.atomic():
             call_command("flush", interactive=False, verbosity=0)
 
-            client, client_2 = self._create_users()
-            self._create_accounts(client, client_2)
-            salary_income = self._create_regular_incomes(client)
-            self._create_regular_expenses(client)
-            self._create_scenario_rules(client, salary_income)
+            self._create_users()
+            self._create_accounts()
+            self._create_regular_incomes()
+            self._create_regular_expenses()
+            self._create_scenario_rules()
 
-            self._calculate_transactions(client, client_2)
+            self._calculate_transactions()
 
-    def _create_users(self) -> tuple[APIClient, APIClient]:
+    def _create_users(self):
         user_model = get_user_model()
         user_model.objects.create_superuser(  # type: ignore[attr-defined]
             username="admin",
@@ -78,79 +84,86 @@ class Bootstraper:
         owner_client = APIClient()
         owner_client.force_authenticate(user=owner)
 
-        stranger = user_model.objects.create_user(  # type: ignore[attr-defined]
-            username="stranger",
-            email="stranger@example.com",
+        demo = user_model.objects.create_user(  # type: ignore[attr-defined]
+            username="demo",
+            email="demo@example.com",
             password="password123",
             first_name="Stranger",
             last_name="Huyanger",
         )
 
         other_client = APIClient()
-        other_client.force_authenticate(user=stranger)
-        return owner_client, other_client
+        other_client.force_authenticate(user=demo)
 
-    def _create_accounts(self, client: APIClient, other_client: APIClient):
+        self.test_client = owner_client
+        self.demo_client = other_client
+
+    def _create_accounts(self):
         payload: dict[str, Any]
-        for payload, client_ in [
+        for payload, client in [
             (
                 {
                     "id": MAIN_ACCOUNT_UUID,
                     "name": "Основной счёт",
-                    "type": AccountType.MAIN.value,
+                    "type": AccountType.MAIN,
                 },
-                client,
+                self.test_client,
             ),
             (
                 {
                     "id": SECOND_ACCOUNT_UUID,
                     "name": "Резерв",
-                    "type": AccountType.RESERVE.value,
+                    "type": AccountType.RESERVE,
                 },
-                client,
+                self.test_client,
             ),
             (
                 {
                     "id": THIRD_ACCOUNT_UUID,
                     "name": "Накопление",
-                    "type": AccountType.ACCUMULATION.value,
+                    "type": AccountType.ACCUMULATION,
                 },
-                client,
+                self.test_client,
             ),
             (
                 {
                     "id": OTHER_ACCOUNT_UUID,
-                    "name": "Накопление",
-                    "type": AccountType.MAIN.value,
+                    "name": "Основной Счёт",
+                    "type": AccountType.MAIN,
                 },
-                other_client,
+                self.demo_client,
             ),
             (
                 {
                     "id": ACCOUNT_UUID_4,
-                    "name": "Счёт 4",
-                    "type": AccountType.ACCUMULATION.value,
+                    "name": "На отпуск",
+                    "type": AccountType.PURPOSE,
+                    "target_amount": "70000.0000",
                 },
-                other_client,
+                self.demo_client,
             ),
             (
                 {
                     "id": ACCOUNT_UUID_5,
-                    "name": "Счёт 5",
-                    "type": AccountType.ACCUMULATION.value,
+                    "name": "Накопления",
+                    "type": AccountType.RESERVE,
                 },
-                other_client,
+                self.demo_client,
             ),
             (
                 {
                     "id": ACCOUNT_UUID_6,
-                    "name": "Счёт 5",
-                    "type": AccountType.ACCUMULATION.value,
+                    "name": "Ипотека",
+                    "type": AccountType.DEBT,
+                    "target_amount": "1000000.0000",
+                    "current_balance": "120000.00"
                 },
-                other_client,
+                self.demo_client,
             ),
         ]:
-            response = client_.post("/api/accounts/", payload, format="json")
+            if client is None:
+                raise ValueError("No client")
+            response = client.post("/api/accounts/", payload, format="json")
             _ensure_success(response, action="create account")
             created_id = response.data.get("id")
             target_id = payload["id"]
@@ -158,105 +171,268 @@ class Bootstraper:
                 # аккуратно с автоматическими присвоениями айди!
                 Account.objects.filter(id=created_id).update(id=target_id)
 
-    def _create_regular_incomes(self, client: APIClient) -> str:
-        income_operations: list[str] = []
+    def _create_regular_incomes(self):
         payload: dict[str, Any]
-        for payload in [
-            {
-                "title": "Зарплата",
-                "description": "Основной доход",
-                "amount": "1000.00",
-                "type": RegularOperationType.INCOME.value,
-                "to_account": MAIN_ACCOUNT_UUID,
-            },
-            {
-                "title": "Фриланс",
-                "description": "Доп. доход",
-                "amount": "500.00",
-                "type": RegularOperationType.INCOME.value,
-                "to_account": MAIN_ACCOUNT_UUID,
-            },
+        for client, payload, attr_name in [
+            (
+                self.test_client,
+                {
+                    "title": "Зарплата",
+                    "description": "Основной доход",
+                    "amount": "1000.00",
+                    "type": RegularOperationType.INCOME,
+                    "start_date": DEFAULT_TIME.isoformat(),
+                    "to_account": MAIN_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.DAY,
+                    "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
+                },
+                "test_salary_scenario_id",
+            ),
+            (
+                self.test_client,
+                {
+                    "title": "Фриланс",
+                    "description": "Доп. доход",
+                    "amount": "500.00",
+                    "type": RegularOperationType.INCOME,
+                    "start_date": DEFAULT_TIME.isoformat(),
+                    "to_account": MAIN_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.DAY,
+                    "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
+                },
+                None,
+            ),
+            (
+                self.demo_client,
+                {
+                    "title": "Аванс",
+                    "description": "К2",
+                    "amount": "100000.00",
+                    "type": RegularOperationType.INCOME,
+                    "start_date": DEFAULT_TIME.isoformat(),
+                    "to_account": OTHER_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.MONTH,
+                },
+                "demo_advance_scenario_id",
+            ),
+            (
+                self.demo_client,
+                {
+                    "title": "Зарплата",
+                    "description": "К2",
+                    "amount": "120000.00",
+                    "type": RegularOperationType.INCOME,
+                    "start_date": (DEFAULT_TIME + timedelta(days=15)).isoformat(),
+                    "to_account": OTHER_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.MONTH,
+                },
+                "demo_salary_scenario_id",
+            ),
+            (
+                self.demo_client,
+                {
+                    "title": "Стипендия",
+                    "description": "РТУ МИРЭА",
+                    "amount": "5000.00",
+                    "type": RegularOperationType.INCOME,
+                    "start_date": (DEFAULT_TIME + timedelta(days=5)).isoformat(),
+                    "to_account": OTHER_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.MONTH,
+                },
+                "demo_scholarship_scenario_id",
+            ),
         ]:
+            if client is None:
+                raise ValueError("No client")
             payload = {
                 **payload,
-                "start_date": DEFAULT_TIME.isoformat(),
-                "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
-                "period_type": RegularOperationPeriodType.DAY,
                 "period_interval": 1,
-                "active_before": date.max,
             }
             response = client.post("/api/regular-operations/", payload, format="json")
             _ensure_success(response, action="create income regular operation")
             scenario = response.data.get("scenario")
             if scenario is None:
                 raise RuntimeError("Scenario is missing for the created regular operation")
-            income_operations.append(scenario["id"])
-        return income_operations[0]
+            if attr_name is not None:
+                setattr(self, attr_name, scenario["id"])
 
-    def _create_regular_expenses(self, client: APIClient):
+    def _create_regular_expenses(self):
         payload: dict[str, Any]
-        for payload in [
-            {
-                "title": "Повседневные траты",
-                "description": "",
-                "amount": "100.00",
-                "type": RegularOperationType.EXPENSE.value,
-            },
-            {
-                "title": "Питание",
-                "description": "",
-                "amount": "50.00",
-                "type": RegularOperationType.EXPENSE.value,
-            },
+        for client, payload in [
+            (
+                self.test_client,
+                {
+                    "title": "Повседневные траты",
+                    "description": "",
+                    "amount": "100.00",
+                    "type": RegularOperationType.EXPENSE,
+                    "from_account": MAIN_ACCOUNT_UUID,
+                    "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
+                    "period_type": RegularOperationPeriodType.DAY,
+                },
+            ),
+            (
+                self.test_client,
+                {
+                    "title": "Питание",
+                    "description": "",
+                    "amount": "50.00",
+                    "type": RegularOperationType.EXPENSE,
+                    "from_account": MAIN_ACCOUNT_UUID,
+                    "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
+                    "period_type": RegularOperationPeriodType.DAY,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "title": "Питание",
+                    "description": "",
+                    "amount": "10000.00",
+                    "type": RegularOperationType.EXPENSE,
+                    "from_account": OTHER_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.MONTH,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "title": "Квартплата",
+                    "description": "",
+                    "amount": "50000.00",
+                    "type": RegularOperationType.EXPENSE,
+                    "from_account": OTHER_ACCOUNT_UUID,
+                    "period_type": RegularOperationPeriodType.MONTH,
+                },
+            ),
         ]:
+            if client is None:
+                raise ValueError("No client")
             payload = {
                 **payload,
-                "from_account": MAIN_ACCOUNT_UUID,
                 "start_date": DEFAULT_TIME.isoformat(),
-                "end_date": DEFAULT_TIME_WITH_OFFSET.isoformat(),
-                "period_type": RegularOperationPeriodType.DAY,
                 "period_interval": 1,
-                "active_before": date.max,
             }
             response = client.post("/api/regular-operations/", payload, format="json")
             _ensure_success(response, action="create expense regular operation")
 
-    def _create_scenario_rules(self, client: APIClient, income_operation_id: str):
+    def _create_scenario_rules(self):
         scenario_rules_payloads = [
-            {
-                "scenario": income_operation_id,
-                "target_account": SECOND_ACCOUNT_UUID,
-                "type": RuleType.FIXED.value,
-                "amount": "200.00",
-                "order": 1,
-            },
-            {
-                "scenario": income_operation_id,
-                "target_account": THIRD_ACCOUNT_UUID,
-                "type": RuleType.FIXED.value,
-                "amount": "300.00",
-                "order": 2,
-            },
-            {
-                "scenario": income_operation_id,
-                "target_account": SECOND_ACCOUNT_UUID,
-                "type": RuleType.FIXED.value,
-                "amount": "100.00",
-                "order": 1,
-            },
+            (
+                self.test_client,
+                {
+                    "scenario": self.test_salary_scenario_id,
+                    "target_account": SECOND_ACCOUNT_UUID,
+                    "type": RuleType.FIXED,
+                    "amount": "200.00",
+                    "order": 1,
+                },
+            ),
+            (
+                self.test_client,
+                {
+                    "scenario": self.test_salary_scenario_id,
+                    "target_account": THIRD_ACCOUNT_UUID,
+                    "type": RuleType.FIXED,
+                    "amount": "300.00",
+                    "order": 2,
+                },
+            ),
+            (
+                self.test_client,
+                {
+                    "scenario": self.test_salary_scenario_id,
+                    "target_account": SECOND_ACCOUNT_UUID,
+                    "type": RuleType.FIXED,
+                    "amount": "100.00",
+                    "order": 1,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_advance_scenario_id,
+                    "target_account": ACCOUNT_UUID_4,
+                    "type": RuleType.FIXED,
+                    "amount": "5000.00",
+                    "order": 1,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_salary_scenario_id,
+                    "target_account": ACCOUNT_UUID_4,
+                    "type": RuleType.FIXED,
+                    "amount": "5000.00",
+                    "order": 1,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_advance_scenario_id,
+                    "target_account": ACCOUNT_UUID_5,
+                    "type": RuleType.FIXED,
+                    "amount": "20000.00",
+                    "order": 2,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_salary_scenario_id,
+                    "target_account": ACCOUNT_UUID_5,
+                    "type": RuleType.FIXED,
+                    "amount": "20000.00",
+                    "order": 2,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_advance_scenario_id,
+                    "target_account": ACCOUNT_UUID_6,
+                    "type": RuleType.FIXED,
+                    "amount": "20000.00",
+                    "order": 3,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_salary_scenario_id,
+                    "target_account": ACCOUNT_UUID_6,
+                    "type": RuleType.FIXED,
+                    "amount": "20000.00",
+                    "order": 3,
+                },
+            ),
+            (
+                self.demo_client,
+                {
+                    "scenario": self.demo_scholarship_scenario_id,
+                    "target_account": ACCOUNT_UUID_6,
+                    "type": RuleType.FIXED,
+                    "amount": "5000.00",
+                    "order": 1,
+                },
+            ),
         ]
 
-        for payload in scenario_rules_payloads:
+        for client, payload in scenario_rules_payloads:
+            if client is None:
+                raise ValueError("No client")
             response = client.post("/api/scenarios/rules/", payload, format="json")
             _ensure_success(response, action="create scenario rule")
 
     def _calculate_transactions(
         self,
-        client: APIClient,
-        other_client: APIClient,
     ):
-        for client_ in [client, other_client]:
-            client_.post("/api/transactions/calculate/")
+        for client in [self.test_client, self.demo_client]:
+            if client is None:
+                raise ValueError("No client")
+            client.post("/api/transactions/calculate/")
 
 
 # def bootstrap_dev_data() -> None:
